52e4a6d9744620c5d2c626d576f5b591f3a4e0dc
zwphyro <tofumaks@gmail.com>
2023-06-30 14:27:01 +0300

    try

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <QApplication>
 #include <QPushButton>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QApplication a(argc, argv);
     QPushButton button("Hello world!", nullptr);
     button.resize(200, 100);
