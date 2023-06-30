67a9d5db52d58c775b79f4e99bbf3991a4db4a62
Margo63 <margo-vinograd@mail.ru>
2023-06-30 14:29:52 +0300

    test_commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <QApplication>
 #include <QPushButton>
-//test_commit
+//test commit
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     QPushButton button("Hello world!", nullptr);
