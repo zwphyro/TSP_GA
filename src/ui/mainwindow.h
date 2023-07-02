a1e4a085de5002a0234c34bf4d321613e71db848
Margo63 <margo-vinograd@mail.ru>
2023-07-02 23:13:15 +0300

    fix variables

--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -2,10 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include<iostream>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+
 
 class MainWindow : public QMainWindow
 {
@@ -15,10 +18,13 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-//private slots:
-//    void on_pushButton_clicked();
+private slots:
+    void on_pushButton_add_edge_clicked();
+    void on_pushButton_generate_add_clicked();
 
 private:
     Ui::MainWindow *ui;
+    std::vector<std::pair<int,std::pair<int,int>>> list;
+    std::vector<std::vector<int>> graph_t;
 };
 #endif // MAINWINDOW_H
