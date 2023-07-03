8ae8bb844f5f2f81e37af56f6add4f0fb58241bf
zwphyro <90712794+zwphyro@users.noreply.github.com>
2023-07-03 17:15:27 +0300

    Revert "add debug, file, scroll"

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -2,29 +2,19 @@
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-        : QMainWindow(parent)
-        , ui(new Ui::MainWindow)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    layout = new QVBoxLayout(this);
-    ui->scrollAreaWidgetContents->setLayout(layout);
 
-//    connect(ui->pushButton_add_edge,SIGNAL(clicked()),
-//            this, SLOT(on_pushButton_add_edge_clicked));
-//    connect(ui->pushButton_generate_add,SIGNAL(clicked()),
-//            this, SLOT(on_pushButton_generate_add_clicked));
-//    connect(ui->pushButton_choose_file,SIGNAL(clicked()),
-//            this, SLOT(onFileChoose()));
+    connect(ui->pushButton_add_edge,SIGNAL(clicked()),
+            this, SLOT(on_pushButton_add_edge_clicked));
 
-    initConnectTabFile();
-    initConnectBlockControl();
-    initConnectTabAddVertex();
-    initConnectTabRandom();
 
-    ui->scrollArea_list_of_edge->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
-    ui->scrollArea_list_of_edge->setWidgetResizable(true);
 
+    connect(ui->pushButton_generate_add,SIGNAL(clicked()),
+            this, SLOT(on_pushButton_generate_add_clicked));
 
 }
 
@@ -34,22 +24,15 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::onPushButtonAddEdgeClicked() {
-    std::pair<int,std::pair<int,int>> element =
-            std::pair(ui->spinBox_distance_between_cities->value(),
-                      std::pair(ui->spinBox_departure_city->value(),ui->spinBox_arrival_city->value()));
+void MainWindow::on_pushButton_add_edge_clicked() {
+    list.push_back(std::pair(ui->spinBox_distance_between_cities->value(),
+                   std::pair(ui->spinBox_arrival_city->value(),ui->spinBox_departure_city->value())));
 
-    list_edge.push_back(element);
-    QString added_line = QString::number(ui->spinBox_departure_city->value()) + " -> "
-                         + QString::number(ui->spinBox_arrival_city->value()) + ": weight = "
-                         + QString::number(ui->spinBox_distance_between_cities->value());
 
-    QLabel *label_added = new QLabel(added_line);
-    layout->addWidget(label_added);
 }
 
 void MainWindow::on_pushButton_generate_add_clicked(){
-    const int n = ui->spinBox_enter_city_add->value();
+    const int n = ui->spinBox_enter_city_add_2->value();
     std::cout << n;
     graph_t.resize(n);
     for(int i = 0; i < n; i++){
@@ -63,9 +46,9 @@ void MainWindow::on_pushButton_generate_add_clicked(){
         std::cout<<"\n";
     }
 
-    for(int i = 0; i < list_edge.size(); i++){
-        if(list_edge[i].second.first < n && list_edge[i].second.second < n) {
-            graph_t[list_edge[i].second.first][list_edge[i].second.second] = list_edge[i].first;
+    for(int i = 0; i < list.size(); i++){
+        if(list[i].second.first < n && list[i].second.second < n) {
+            graph_t[list[i].second.first][list[i].second.second] = list[i].first;
         }
     }
 
@@ -75,54 +58,7 @@ void MainWindow::on_pushButton_generate_add_clicked(){
         }
         std::cout<<"\n";
     }
+
+
+
 }
-
-
-void MainWindow::onFileChoose(){
-    QString str = QFileDialog::getOpenFileName(0, "Выбор папки", "","*txt");
-    ui->label_chosen_file->setText(str);
-}
-
-void MainWindow::initConnectTabFile(){
-    connect(ui->pushButton_choose_file,SIGNAL(clicked()),
-            this, SLOT(onFileChoose()));
-    connect(ui->pushButton_generate_file,SIGNAL(clicked()),
-            this,SLOT(onGraphGenerate()));
-}
-void MainWindow::initConnectTabAddVertex() {
-    connect(ui->pushButton_generate_add,SIGNAL(clicked()),
-            this,SLOT(onGraphGenerate()));
-
-    connect(ui->pushButton_add_edge,SIGNAL(clicked()),
-            this,SLOT(onPushButtonAddEdgeClicked()));
-}
-
-void MainWindow::initConnectTabRandom() {
-
-    connect(ui->pushButton_full_rnd,SIGNAL(clicked()),
-            this,SLOT(onGraphGenerate()));
-
-    connect(ui->pushButton_generate_rnd,SIGNAL(clicked()),
-            this,SLOT(onGraphGenerate()));
-}
-
-void MainWindow::initConnectBlockControl() {
-    connect(ui->pushButton_skip,SIGNAL(clicked()),
-            this,SLOT(onGraphGenerate()));
-
-    connect(ui->pushButton_start,SIGNAL(clicked()),
-            this,SLOT(onGraphGenerate()));
-
-    connect(ui->pushButton_step_back,SIGNAL(clicked()),
-            this,SLOT(onGraphGenerate()));
-
-    connect(ui->pushButton_step_forward,SIGNAL(clicked()),
-            this,SLOT(onGraphGenerate()));
-}
-
-void MainWindow::onGraphGenerate() {
-
-    std::cout<<"work\n";
-}
-
-
--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -2,10 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QFileDialog>
-#include <QDebug>
 #include<iostream>
-#include <QVBoxLayout>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,27 +12,19 @@ QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-Q_OBJECT
+    Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
-    void onPushButtonAddEdgeClicked();
+    void on_pushButton_add_edge_clicked();
     void on_pushButton_generate_add_clicked();
-    void onFileChoose();
-    void onGraphGenerate();
 
 private:
     Ui::MainWindow *ui;
-    std::vector<std::pair<int,std::pair<int,int>>> list_edge;
+    std::vector<std::pair<int,std::pair<int,int>>> list;
     std::vector<std::vector<int>> graph_t;
-    QVBoxLayout *layout;
-
-    void initConnectTabFile();
-    void initConnectTabAddVertex();
-    void initConnectTabRandom();
-    void initConnectBlockControl();
 };
 #endif // MAINWINDOW_H
