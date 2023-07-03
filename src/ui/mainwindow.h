b1a6ae21c52c88ab64a810dc7f13b0a68c725c43
Margo63 <margo-vinograd@mail.ru>
2023-07-03 17:00:09 +0300

    add debug, file, scroll

--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -2,7 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QDebug>
 #include<iostream>
+#include <QVBoxLayout>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -12,19 +15,27 @@ QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
-    void on_pushButton_add_edge_clicked();
+    void onPushButtonAddEdgeClicked();
     void on_pushButton_generate_add_clicked();
+    void onFileChoose();
+    void onGraphGenerate();
 
 private:
     Ui::MainWindow *ui;
-    std::vector<std::pair<int,std::pair<int,int>>> list;
+    std::vector<std::pair<int,std::pair<int,int>>> list_edge;
     std::vector<std::vector<int>> graph_t;
+    QVBoxLayout *layout;
+
+    void initConnectTabFile();
+    void initConnectTabAddVertex();
+    void initConnectTabRandom();
+    void initConnectBlockControl();
 };
 #endif // MAINWINDOW_H
