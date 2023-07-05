5393bf77f0de6245dd1d312ed9509a40378c8844
zwphyro <tofumaks@gmail.com>
2023-07-05 15:38:53 +0300

    mating polished

--- src/mating/mating.h
+++ src/mating/mating.h
@@ -7,15 +7,16 @@
 class Mating
 {
 public:
-    Mating(int chromosome_size, double mutation_probability, int amount_of_crossover_dots);
-    std::vector<chromosome_t> getChildPopulation(const Population &population);
+    Mating(int chromosome_size, int amount_of_crossover_dots, double mutation_probability);
+    std::vector<chromosome_t> getChildren(const Population &population);
 
 private:
-    chromosome_t ordered_crossover(const chromosome_t &parent_1, const chromosome_t &parent_2, int crossover_start,
-                                   int crossover_end);
+    chromosome_t
+    ordered_crossover(const chromosome_t &first_parent, const chromosome_t &second_parent, int crossover_start,
+                      int crossover_end);
 
-    chromosome_t mutation(const chromosome_t &chromosome, int mutation_rate);
-    chromosome_t mutation_switch(const chromosome_t &chromosome, int mutation_rate);
+    chromosome_t mutation(chromosome_t &chromosome, int mutation_rate);
+    chromosome_t mutation_switch(chromosome_t &chromosome, int mutation_rate);
 
     int chromosome_size;
     int amount_of_crossover_dots;
