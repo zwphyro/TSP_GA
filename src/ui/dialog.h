37ced1040737c976bceb7e3d320005906b0da9de
zwphyro <tofumaks@gmail.com>
2023-07-06 18:04:23 +0300

    ui autogen fixed

--- src/ui/dialog.h
+++ src/ui/dialog.h
@@ -12,10 +12,14 @@
 
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class Dialog; }
+namespace Ui
+{
+    class Dialog;
+}
 QT_END_NAMESPACE
 
-class Dialog : public QDialog {
+class Dialog : public QDialog
+{
 Q_OBJECT
 
 public:
